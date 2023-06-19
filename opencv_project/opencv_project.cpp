08cec10431a0933fc643f426e43d06f9d80f4c60
vartomic <varrava16@mail.ru>
2023-06-19 20:56:00 +0300

    Add new empty window, add picture

--- opencv_project/opencv_project.cpp
+++ opencv_project/opencv_project.cpp
@@ -1,7 +1,45 @@
-﻿#include <iostream>
+﻿#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	/*//Create window
+	cv::namedWindow("New_window", WINDOW_NORMAL);
 
+	//Resize window
+	cv::resizeWindow("New_window", 800, 480);
+
+	int brightness_slider = 50;
+
+	//Add slider "brightness"
+	createTrackbar("Brightness", "New_window", &brightness_slider, 100);*/
+
+	Mat lena = imread("img/z_96977774.jpg");
+	if (!lena.data)
+	{
+		cout << "Image is missing" << endl;
+		return -1;
+	}
+
+	cv::namedWindow("New_window", WINDOW_NORMAL);
+
+	//Display window
+	imshow("New_window", lena);
+
+	cv::resizeWindow("New_window", 511, 682);
+
+	int brightness_slider = 50;
+
+	//Add slider "brightness"
+	createTrackbar("Brightness", "New_window", &brightness_slider, 100);
+
+	//Press key to close a window
+	waitKey(0);
+
+	return 0;
+}
